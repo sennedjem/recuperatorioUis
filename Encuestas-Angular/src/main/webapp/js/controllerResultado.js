d25d3be8f4e00c48fc7a50e7fe71f04949851fac
sennedjem <sebastianariell@hotmail.com.ar>
2015-12-09 02:29:39 -0300

    Delete controllerResultado.js

--- Encuestas-Angular/src/main/webapp/js/controllerResultado.js
+++
@@ -1,6 +0,0 @@
-angular.module('ganasteApp')
-.controller('ResultadoController', function($scope, ServiceResultado) { 
-  $scope.resultadoDelJuego = ServiceResultado.resultadoDelJuego();
-  $scope.ganador=ServiceResultado.datosDelJuego().iniciador;
-  $scope.ganador=ServiceResultado.datosDelJuego().retado;
-  });
